75c66eda0d7f7f429f1229c259f482e82f829894
Matt Berg <berg@fujisan.co.jp>
2019-03-22 17:19:25 -0700

    mobile directory with working script

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ var express = require('express'),
     path = require('path'),
     app = express();
 
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('build'));
-
-app.get('/mobile', function (req, res) {
-  res.send('see this site on a computer plz')
-})
 
 app.listen(app.get('port'), function(err) {
   if (err) {
